e543f7cf399ad3e0d5792e8d46b0f8fb56c1737a
AlexC1991 <114543336+AlexC1991@users.noreply.github.com>
2024-02-13 14:56:30 +1100

    Updated Cluster Organiser

--- Assets/Scripts/DestructionScript.cs
+++ Assets/Scripts/DestructionScript.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,6 +9,7 @@ namespace AlexzanderCowell
 {
     public class DestructionScript : MonoBehaviour
     {
+        [Header("Tile Grid Placement")]
         [Range(-100, 1000)] [SerializeField] private int gridSizeX;
         [Range(-100, 1000)] [SerializeField] private int gridSizeY;
         [Range(-1, 3)] [SerializeField] private float cellSize;
@@ -15,22 +18,42 @@ namespace AlexzanderCowell
         private int cellSizingX;
         private int cellSizingY;
         private int[,] grid; // Represents the state of each grid cell
-        [Header("Tile Prefab")]
+        [Header("Most Common Blocks")]
         [SerializeField] private GameObject[] mostSeenLayer;
         [SerializeField] private GameObject grassDirtLayer;
+        [Header("None Common Ore's")]
         [SerializeField] private GameObject[] oreLayer;
+        [Header("Copper Ore & Commons")]
+        [SerializeField] private GameObject[] copperOreOptions;
+        [Header("Steel Ore & Commons")]
+        [SerializeField] private GameObject[] steelOreOptions;
+        [Header("Diamond Ore & Commons")]
+        [SerializeField] private GameObject[] diamondOreOptions;
+        [Header("BlocksOnScene")]
+        [SerializeField] private GameObject[] blockChildren;
+        [SerializeField] private GameObject parentOfBlockChildren;
+        [Header("Sprites Of Each Ore")]
+        [SerializeField] private Sprite copperExample;
+        [SerializeField] private Sprite steelExample;
+        [SerializeField] private Sprite diamondExample;
+        [Header("Lists Of Each Ore In Grid")]
+        [SerializeField] private List<GameObject> copperBlocks;
+        [SerializeField] private List<GameObject> steelBlocks;
+        [SerializeField] private List<GameObject> diamondBlocks;
         private Vector3 gridCenter;
         private GameObject spawnTile;
         private Vector3 cellCenter;
         float noiseScale = 0.1f;
         private float offsetX;
         private float offsetY;
+        public bool checkCopperStage;
 
         private void Start()
         {
             offsetX = Random.Range(0f, 99999f);
             offsetY = Random.Range(0f, 99999f);
             StartCoroutine(InitializeGrid());
+            StartCoroutine(CheckCopperOres());
         }
 
         IEnumerator InitializeGrid()
@@ -77,22 +100,101 @@ namespace AlexzanderCowell
                     }
                 }
             }
+            
+            blockChildren = GetAllChildren(parentOfBlockChildren.transform);
+            checkCopperStage = true;
         }
 
-        private void OnDrawGizmosSelected()
+        IEnumerator CheckCopperOres()
         {
-            grid = new int[gridSizeX, gridSizeY];
-            gridCenter = transform.position + new Vector3((gridSizeX - adjustableXLocation) * cellSize / 1, (gridSizeY - adjustableYLocation) * cellSize / 1, 0f);
-            
-            for (int x = cellSizingX; x < gridSizeX; x++)
+            while (true)
             {
-                for (int y = cellSizingY; y < gridSizeY; y++)
+                if (checkCopperStage)
                 {
-                    cellCenter = gridCenter + new Vector3(x * cellSize, y * cellSize, 0f);
-                    Gizmos.color = Color.green;
-                    Gizmos.DrawWireCube(cellCenter, new Vector3(cellSize, cellSize, 0f));
+                    foreach (GameObject g in blockChildren)
+                    {
+                        if (g.GetComponent<SpriteRenderer>().sprite == copperExample)
+                        {
+                            copperBlocks.Add(g);
+                        }
+                    }
+
+                    float spacing = 1.0f; // Replace with your actual spacing between blocks
+
+                    foreach (GameObject copperBlock in copperBlocks)
+                    {
+                        Vector3 position = copperBlock.transform.position;
+                        // Define adjacent positions
+                        Vector3[] adjacentPositions = new Vector3[]
+                        {
+                            position + new Vector3(spacing, 0, 0), // Right
+                            position + new Vector3(-spacing, 0, 0), // Left
+                            position + new Vector3(0, spacing, 0), // Up
+                            position + new Vector3(0, -spacing, 0) // Down
+                        };
+
+                        foreach (Vector3 adjacentPosition in adjacentPositions)
+                        {
+                            GameObject adjacentBlock = FindBlockAtPosition(adjacentPosition);
+                            if (adjacentBlock != null &&
+                                adjacentBlock.GetComponent<SpriteRenderer>().sprite != copperExample)
+                            {
+                                // This is where you apply your logic for replacing the block or whatever you intend to do
+                                // For example, to replace it with a random copper block:
+                                GameObject newBlockPrefab = copperOreOptions[Random.Range(0, copperOreOptions.Length)];
+                                Instantiate(newBlockPrefab, adjacentPosition, Quaternion.identity);
+                                Destroy(adjacentBlock); // Remove the old block
+                            }
+                        }
+                    }
+                    checkCopperStage = false;
                 }
+                
+                yield return null;
             }
         }
+
+
+        private void OnDrawGizmosSelected()
+            {
+                grid = new int[gridSizeX, gridSizeY];
+                gridCenter = transform.position + new Vector3((gridSizeX - adjustableXLocation) * cellSize / 1, (gridSizeY - adjustableYLocation) * cellSize / 1, 0f);
+            
+                for (int x = cellSizingX; x < gridSizeX; x++)
+                {
+                    for (int y = cellSizingY; y < gridSizeY; y++)
+                    {
+                        cellCenter = gridCenter + new Vector3(x * cellSize, y * cellSize, 0f);
+                        Gizmos.color = Color.green;
+                        Gizmos.DrawWireCube(cellCenter, new Vector3(cellSize, cellSize, 0f));
+                    }
+                }
+            }
+        
+            private GameObject[] GetAllChildren(Transform parent)
+            {
+                int childCount = parent.childCount;
+                GameObject[] children = new GameObject[childCount];
+
+                for (int i = 0; i < childCount; i++)
+                {
+                    Transform child = parent.GetChild(i);
+                    children[i] = child.gameObject;
+                }
+
+                return children;
+            }
+        
+            private GameObject FindBlockAtPosition(Vector3 position)
+            {
+                foreach (GameObject block in blockChildren)
+                {
+                    if (block.transform.position == position) // Consider using approximation due to floating-point precision
+                    {
+                        return block;
+                    }
+                }
+                return null;
+            }
     }
 }
